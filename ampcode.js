a59271c39bad51f838604ef8ab92f05c15f8d3c4
trevorkeithjames <74616839+trevorkeithjames@users.noreply.github.com>
2020-11-19 18:17:02 +0000

    Update ampcode.js

--- ampcode.js
+++ ampcode.js
@@ -10,18 +10,18 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/amp/qualt
 				nameForLogging : 'Black people', //Will be used in the logging
 				//An array of all media objects for this category.
 				mediaArray : [
-				    {image : insect1.jpg}, 
-					{image : insect2.jpg}, 
-					{image : insect3.jpg}, 
-					{image : insect4.jpg}, 
-					{image : insect5.jpg}, 
-					{image : insect6.jpg}, 
-					{image : insect7.jpg}, 
-					{image : insect8.jpg}, 
-					{image : insect1.jpg}, 
-					{image : insect2.jpg}, 
-					{image : insect3.jpg}, 
-					{image : insect4.jpg}]
+				    {image : insect1.jpg }, 
+					{image : insect2.jpg }, 
+					{image : insect3.jpg }, 
+					{image : insect4.jpg }, 
+					{image : insect5.jpg }, 
+					{image : insect6.jpg }, 
+					{image : insect7.jpg }, 
+					{image : insect8.jpg }, 
+					{image : insect1.jpg }, 
+					{image : insect2.jpg }, 
+					{image : insect3.jpg }, 
+					{image : insect4.jpg }]
 
 			}, 
 			{
@@ -29,18 +29,18 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/amp/qualt
 				nameForLogging : 'White people', //Will be used in the logging
 				//An array of all media objects for this category.
 				mediaArray : [
-				    {image : flower1.jpg}, 
-					{image : flower2.jpg}, 
-					{image : flower3.jpg}, 
-					{image : flower4.jpg}, 
-					{image : flower5.jpg}, 
-					{image : flower6.jpg}, 
-					{image : flower7.jpg}, 
-					{image : flower8.jpg}, 
-					{image : flower1.jpg}, 
-					{image : flower2.jpg}, 
-					{image : flower3.jpg}, 
-					{image : flower4.jpg}]
+				    {image : flower1.jpg }, 
+					{image : flower2.jpg }, 
+					{image : flower3.jpg }, 
+					{image : flower4.jpg }, 
+					{image : flower5.jpg }, 
+					{image : flower6.jpg }, 
+					{image : flower7.jpg }, 
+					{image : flower8.jpg }, 
+					{image : flower1.jpg }, 
+					{image : flower2.jpg }, 
+					{image : flower3.jpg }, 
+					{image : flower4.jpg }]
 			}
 		],
 
