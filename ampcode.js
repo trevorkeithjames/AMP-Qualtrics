e00ab61891e43d6be31402343f8b2cfbfcfe3474
trevorkeithjames <74616839+trevorkeithjames@users.noreply.github.com>
2020-11-19 17:43:28 +0000

    Update ampcode.js

--- ampcode.js
+++ ampcode.js
@@ -52,7 +52,7 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/amp/qualt
 		},
 
 		base_url : {//Where are your images at?
-			image : 'https://baranan.github.io/minno-tasks/images/ampImages'
+			image : 'https://github.com/trevorkeithjames/AMP-Qualtrics/images'
 		}
 	});
 });
