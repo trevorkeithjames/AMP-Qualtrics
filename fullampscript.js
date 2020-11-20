eba05e0bd54d1b3338a0919e743b5d2ae378463d
trevorkeithjames <74616839+trevorkeithjames@users.noreply.github.com>
2020-11-20 16:38:20 +0000

    Update fullampscript.js

--- fullampscript.js
+++ fullampscript.js
@@ -123,7 +123,7 @@ define(['pipAPI','underscore'], function(APIConstructor, _) {
 			leftKey : 'e', 
 			
 			base_url : {//Where are your images at?
-				image : 'https://trevorkeithjames.github.io/AMP-Qualtrics/ampimages'
+				image : 'https://baranan.github.io/minno-tasks/images/ampImages'
 			}, 
 
 			trialsInBlock : [40, 40, 40], //Number of trials in each block 
